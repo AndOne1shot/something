e546d4962bf910b89317bb68c6abcf1122e6e4da
AndOne1shot <wnduddud97@naver.com>
2024-09-04 17:36:47 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Home from "./Home";
+import About from "./About";
+
+function App() {
+  return (
+  <BrowserRouter>
+    <h1>hi react</h1>
+    <ul>
+      <li><Link to="/">홈</Link></li>
+      <li><Link to="/about">소개</Link></li>
+    </ul><hr />
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
+    
+  
+  </BrowserRouter>
+  );
+}
+
+export default App;
