7993741822e06c026c0fc2c6817c7d6def098955
AndOne1shot <wnduddud97@naver.com>
2024-09-05 15:30:50 +0900

    Create Test.js

---
+++ react/router2/Test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Test = () => {
+    return(
+        <div>
+            <h1>테스트</h1>
+            <p>테스트 페이지 입니다.</p>
+        </div>
+    );
+}
+export default Test;
